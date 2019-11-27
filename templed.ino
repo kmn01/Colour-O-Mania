573bc3412bdc1287388c4c7e6098095529411e98
kmn01 <55681293+kmn01@users.noreply.github.com>
2019-11-27 22:42:42 +0530

    Add files via upload

---
+++ templed.ino
@@ -0,0 +1,19 @@
+int red=50;
+//int green=46;
+//int blue=44;
+
+void setup() 
+{
+   pinMode(red, OUTPUT);
+   //pinMode(green, OUTPUT);
+   //pinMode(blue, OUTPUT);
+}
+void loop() 
+{
+  //digitalWrite(blue, HIGH);
+  //delay(2000);
+  //digitalWrite(green, HIGH);
+  //delay(2000);
+  digitalWrite(red, HIGH);
+  delay(2000);
+}
